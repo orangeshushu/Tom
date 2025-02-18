949f4ff31566ecb719c92c631e672ccfa2c1f053
orangeshushu <751563925@qq.com>
2025-02-17 20:44:39 -0600

    Add extra base64 image from JSON files

--- covert2image.py
+++ covert2image.py
@@ -5,32 +5,44 @@ import json
 import os
 
 path = 'test_img'
+output_folder = "test_img_res"
 # path = 'raw_image_before1000'
-for root, dirs, files in os.walk(path):
-    for file in files:
-        filepath = root + "/" + file
-        name = file[:4]
-        print(name)
-        # 创建一个1080*1440的纯黑色画布
-        canvas = Image.new('RGB', (1080, 1440), (0, 0, 0))
+for root_folder, each_folder, root_files in os.walk(path):
+    print(root_folder, each_folder)
+    break
+for i in each_folder:
+    whole_path = os.path.join(root_folder, i)
+    output_path = os.path.join(output_folder, i)
+    for root, dirs, files in os.walk(whole_path):
+        for file in files:
+            filepath = os.path.join(root, file)
 
-        # 打开并读取JSON文件
-        with open(filepath, 'r', encoding='UTF-8') as file:
-            data = json.load(file)
+            # 解析文件名（去掉扩展名）
+            name = os.path.splitext(file)[0]
 
-        # 获取'imageData'项的内容
-        base64_image_string = data['shapes'][0]['mask']
+            # 创建一个1080*1440的纯黑色画布
+            canvas = Image.new('RGB', (1080, 1440), (0, 0, 0))
 
-        position = data['shapes'][0]['points'][0]
-        # base64_image_string = ''
+            # 打开并读取 JSON 文件
+            with open(filepath, 'r', encoding='UTF-8') as json_file:
+                data = json.load(json_file)
 
-        image_data = base64.b64decode(base64_image_string)
+            # 解析 JSON 获取 imageData 和 位置
+            base64_image_string = data['shapes'][0]['mask']
+            position = data['shapes'][0]['points'][0]
 
-        image = Image.open(BytesIO(image_data))
+            # 解码 base64 图片
+            image_data = base64.b64decode(base64_image_string)
+            image = Image.open(BytesIO(image_data))
 
-        # 将1.jpg放置在指定的(480, 640)坐标位置上
-        canvas.paste(image, (int(position[0]), int(position[1])))
+            # 将解码的图片粘贴到指定位置
+            canvas.paste(image, (int(position[0]), int(position[1])))
 
-        # 保存合成后的图片为predict.jpg
-        # canvas.save('pred_mask/ground truth/{filename}.jpg'.format(filename=name))
-        canvas.save('test_img_res/{filename}.jpg'.format(filename=name))
+            # **确保目标目录存在**
+            save_folder = output_path
+            os.makedirs(save_folder, exist_ok=True)  # 创建目录（如果不存在）
+
+            # **保存图片**
+            save_path = os.path.join(save_folder, f"{name}.jpg")
+            canvas.save(save_path)
+            print(f"图片已保存：{save_path}")
